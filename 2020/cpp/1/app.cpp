97878249a590a2983ba6c51331251b601ec07eb5
julitopower <julio.delgadomangas@gmail.com>
2020-12-04 09:48:55 -0800

    Day1-2 solution
    Implemented a generalized algorithm that is able to find sequencies
    of arbitrary size that sum up to a target number. With this algorithm
    we can solve both day1-1 and day1-2.

--- 2020/cpp/1/app.cpp
+++ 2020/cpp/1/app.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <numeric>
 
 std::vector<int> load_data(const std::string& filename) {
   auto fs = std::fstream{filename};
@@ -13,20 +14,49 @@ std::vector<int> load_data(const std::string& filename) {
   return output;
 }
 
+bool swipe(int level, int n, int start, const std::vector<int>& data, int target, std::vector<int>& result) {
+  int end = data.size() - (n - level);
+  for (auto idx = start ; idx < end ; ++idx) {
+    // Set the value for the current level idx
+    result [level] = data[idx];
+    // If this is the last level compute the sum and compare with target value
+    if (level == n) {
+      int accu = std::accumulate(result.begin(), result.end(), 0);
+      if (accu == target) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    // Otherwise, swipe over the values in the next level, and break
+    // the iteration if the next level reports that it found a solution
+    if (swipe(level + 1, n, idx + 1, data, target, result)) {
+      return true;
+    }
+  }
+
+  // If we are here, we didn't find a solution
+  return false;
+}
+
+std::vector<int> findn(const std::vector<int> input, int n, int target) {
+  std::vector<int> candidates(n, 0);
+  swipe(0, n, 0, input, target, candidates);
+  return candidates;
+}
+
 int main(int argc, char** argv) {
     std::cout << "Advent of code 2020. Day 1" << std::endl;
 
     const std::vector<int> data = load_data("./input.txt");
-
     const int target_value = 2020;
-    // N^2 traversal to find matching pair
-    int offset = 0;
-    for (int v1 : data) {
-      for (auto it = data.begin() + offset ; it != (data.end() - 1) ; ++it) {
-        if ((v1 + *it) == target_value) {
-          std::cout << (v1 * (*it)) << std::endl;
-        }
-      }
-      ++offset;
+
+    // Find 2 and 3 elements that sum to target_value
+    for (auto n : {2, 3}) {
+      const auto values = findn(data, n, target_value);
+      auto value = std::accumulate(values.begin(), values.end(), 1, [](int a, int b) { return a * b; });
+      std::cout << "The product of " << n << " values that add to " << target_value
+                << " is " << value << std::endl;
     }
 }
