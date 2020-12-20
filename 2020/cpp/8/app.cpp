1db23f2dab7c75cb8ed7cd73719fe7a4e46c8131
Julio Delgado Mangas <julio.delgadomangas@gmail.com>
2020-12-20 20:34:39 +0000

    Formating day8 code

--- 2020/cpp/8/app.cpp
+++ 2020/cpp/8/app.cpp
@@ -1,69 +1,61 @@
-#include <iostream>
 #include <fstream>
-#include <vector>
-#include <string>
-#include <sstream>
+#include <iostream>
 #include <set>
+#include <sstream>
+#include <string>
+#include <vector>
 
-enum class Ops {
-  ACC,
-  JMP,
-  NOOP
-};
+enum class Ops { ACC, JMP, NOOP };
 
 class Memory {
- public:
+public:
   void add_instruction(Ops op_code, int value = 0) {
     mem_.push_back({op_code, value});
   }
 
-  const std::pair<Ops, int>& read(std::size_t idx) const {
+  const std::pair<Ops, int> &read(std::size_t idx) const {
     return mem_.at(idx);
   }
-  
- private:
+
+private:
   std::vector<std::pair<Ops, int>> mem_;
 };
 
 class CPU {
- public:
+public:
   void reset() {
     pc_ = 0;
     acc_ = 0;
-   }
-
-  int accumulator() {
-    return acc_;
   }
 
-  void run_next(const Memory& mem) {
+  int accumulator() { return acc_; }
+
+  void run_next(const Memory &mem) {
     auto inst = mem.read(pc_);
     switch (inst.first) {
-      case Ops::ACC:
-        acc_ += inst.second;
-        ++pc_;
-        break;
-      case Ops::JMP:
-        pc_ += inst.second;
-        break;
-      case Ops::NOOP:
-        ++pc_;
-        break;
+    case Ops::ACC:
+      acc_ += inst.second;
+      ++pc_;
+      break;
+    case Ops::JMP:
+      pc_ += inst.second;
+      break;
+    case Ops::NOOP:
+      ++pc_;
+      break;
     }
   }
 
-  std::size_t pc() const {
-    return pc_;
-  }
-  
- private:
+  std::size_t pc() const { return pc_; }
+
+private:
   std::size_t pc_ = 0;
   int acc_ = 0;
 };
 
-void load_program(const std::string& filename, Memory& mem) {
+void load_program(const std::string &filename, Memory &mem) {
   std::ifstream fs{filename};
-  for (std::string line ; std::getline(fs, line) ; ) {
+  for (std::string line; std::getline(fs, line);) {
     std::stringstream ss{line};
     std::string op;
     int value;
@@ -73,14 +65,14 @@ void load_program(const std::string& filename, Memory& mem) {
       mem.add_instruction(Ops::ACC, value);
     } else if (op == "jmp") {
       ss >> value;
-      mem.add_instruction(Ops::JMP, value);      
+      mem.add_instruction(Ops::JMP, value);
     } else if (op == "nop") {
-      mem.add_instruction(Ops::NOOP);      
+      mem.add_instruction(Ops::NOOP);
     }
   }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   Memory mem{};
   CPU cpu{};
   load_program("input8.txt", mem);
